201f3ede64554349362e455edc515b72023a9d8a
ruei <ruei@null.computer>
2021-04-19 19:54:26 +0800

    :sparkles: udpate vite to v2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import vue from '@vitejs/plugin-vue'
+
 const path = require('path');
 
 export default {
-  alias: {
-    '/@/': path.resolve(__dirname, './src')
-  }
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, './src')
+    }
+  },
+  server: {
+    port: 3333,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'debug'
+      }
+    }  
+  } 
 }
