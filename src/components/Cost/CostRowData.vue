0ade84c470d508aaa4e1d0ed6b3366097c9cc514
ruei <ruei@null.computer>
2021-04-28 21:48:15 +0800

    :sparkles: change cost name input to span

--- src/components/Cost/CostRowData.vue
+++ src/components/Cost/CostRowData.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex relative items-center">
     <div class="p-1 flex-1 flex">
-      <input class="p-2 flex-1 outline-none focus:ring-2 focus:ring-green-500 hover:bg-gray-200" type="text" :value="cost.item" @input="updateItem($event, cost.id)">
+      <span class="p-2 flex-1 outline-none " @input="updateItem($event, cost.id)">{{cost.item}}</span>
     </div>
 
     <div class="p-1 flex-1 flex">
@@ -17,7 +17,6 @@
 </template>
 
 <script>
-import { ref } from 'vue';
 import { useCostContext } from '/@/composables/cost';
 
 export default {
